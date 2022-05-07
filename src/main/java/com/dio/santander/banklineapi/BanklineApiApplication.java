83a1691d697f36062b2de7326b21b08c26ca36af
Tiago Pires <tiagoa.p@hotmail.com>
2022-05-07 01:15:43 -0300

    tentando corrigir bug

--- src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
@@ -2,6 +2,10 @@ package com.dio.santander.banklineapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@EntityScan (basePackages = "dio.santander.banklineapi.model")
 
 @SpringBootApplication
 public class BanklineApiApplication {
@@ -10,5 +14,7 @@ public class BanklineApiApplication {
 		SpringApplication.run(BanklineApiApplication.class, args);
 		//vamos iniciar
 	}
+	
+	
 
 }
