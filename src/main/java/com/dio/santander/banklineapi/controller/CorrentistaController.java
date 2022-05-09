e8942c22ae5f5d1078172f573a1c5e549a9c7248
Tiago Pires <tiagoa.p@hotmail.com>
2022-05-09 20:45:03 -0300

    post criado

--- src/main/java/com/dio/santander/banklineapi/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/banklineapi/controller/CorrentistaController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,10 @@ public class CorrentistaController {
 	@GetMapping
 	public List<Correntista> findAll(){
 		return repository.findAll();		
+	}	
+	
+	@PostMapping
+	public void save(@RequestBody Correntista correntista) {
+		
 	}
-	
-	
 }
